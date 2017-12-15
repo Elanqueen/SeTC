b36c01c25ddd5255baa6adf4b5879ab062cf3293
Elanqueen <775254855@qq.com>
2017-12-15 17:55:42 +0800

    add public

---
+++ public/constant.py
@@ -0,0 +1,18 @@
+#constant.py
+#coding=utf-8
+'''常量维护'''
+#=============================邮箱维护=================================
+mail_from ="发送邮箱"
+mail_pwd="授权码" #需提前开通邮箱的SMTP服务
+mail_to = "接收邮箱"
+mail_link_method = 'smtp.163.com'
+
+
+#==========================数据信息维护================================
+
+#u网页登录地址
+LOGIN_URL = 'http://www.bplisn.net.cn/'
+
+#登录信息地址
+XML_LOGIN = 'D:\\Repositories\\SeTC\\data\\xml_login.xml'
+XML_SEARCH = 'D:\\Repositories\\SeTC\\data\\xml_search.xml'
